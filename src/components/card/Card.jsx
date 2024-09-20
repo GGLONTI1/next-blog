f54ef3c2bb1f8cdf49137b35da0c49bf655bc0c0
GGLONTI1 <giorgiglonti23@gmail.com>
2024-09-20 19:54:58 +0400

    write page

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import styles from "./card.module.css"
+import Link from "next/link"
+
+const Card = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.detail}>
+                    <span className={styles.date}>11.02.2024</span>
+                    <span className={styles.category}> - Culture</span>
+                </div>
+                <Link href={"/"}>
+                    <h1>Lorem ipsum dolor, sit amet consectetur elit.</h1>
+                </Link>
+                <p className={styles.desc}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum excepturi hic ea, officia rem ab ipsa iusto neque optio,
+                    repellendus esse vel laboriosam perspiciatis ducimus praesentium deserunt,
+                    adipisci minus reprehenderit?...
+                </p>
+                <Link href={"/"} className={styles.link}>Read More</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Card
