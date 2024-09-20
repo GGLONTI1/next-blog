7a0f1c7db1df5b048457782b732e2b4dde4974ce
GGLONTI1 <giorgiglonti23@gmail.com>
2024-09-20 21:08:52 +0400

    write page done

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -9,11 +9,11 @@ const WritePage = () => {
     const [open, setOpen] = useState(false)
     const [value, setValue] = useState("")
     return (
-        <div className={styles.container}>
-            <input type="text" placeholder="Title" />
+        <div className={styles.container} >
+            <input type="text" placeholder="Title" className={styles.input} />
             <div className={styles.editor}>
-                <button className={styles.button}>
-                    <Image src="/plus.png" alt="" width={16} height={16} />
+                <button className={styles.button} onClick={() => setOpen(!open)}>
+                    <Image src="/plus.png" alt="" width={16} height={16} className={open && styles.plus} />
                 </button>
                 {open && (
                     <div className={styles.add}>
@@ -37,12 +37,14 @@ const WritePage = () => {
                     </div>
                 )}
                 <ReactQuill
+                    className={styles.textArea}
                     theme="bubble"
                     value={value}
                     onChange={setValue}
                     placeholder="Tell your story..."
                 />
             </div>
+            <button className={styles.publish}>Publish</button>
         </div>
     )
 }
