edbfec6ebef403ab52b0a1ce8783ecbd54e5e00c
GGLONTI1 <giorgiglonti23@gmail.com>
2024-09-24 03:59:36 +0400

    Prisma Client Installed

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,19 +1,26 @@
-"use client"
-import Link from "next/link"
-import styles from "./authLinks.module.css"
-import { useState } from "react"
+"use client";
+import Link from "next/link";
+import styles from "./authLinks.module.css";
+import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
-    const [open, setOpen] = useState(false)
-    const status = "authenticated"
+    const [open, setOpen] = useState(false);
+
+    const { status } = useSession();
+
     return (
         <>
-            {status === "notauthenticated" ? (
-                <Link href="/login">Login</Link>
+            {status === "unauthenticated" ? (
+                <Link href="/login" className={styles.link}>
+                    Login
+                </Link>
             ) : (
                 <>
-                    <Link href="/write">Write</Link>
-                    <span className={styles.link}>
+                    <Link href="/write" className={styles.link}>
+                        Write
+                    </Link>
+                    <span className={styles.link} onClick={signOut}>
                         Logout
                     </span>
                 </>
@@ -39,7 +46,7 @@ const AuthLinks = () => {
                 </div>
             )}
         </>
-    )
-}
+    );
+};
 
 export default AuthLinks;
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -10,9 +10,6 @@ import { ThemeContext } from "@/context/ThemeContext";
 const ThemeToggle = () => {
 
     const { toggle, theme } = useContext(ThemeContext)
-    console.log(theme);
-
-
 
     return (
         <div className={styles.container} onClick={toggle} style={
