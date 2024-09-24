4f51d008902dce1fc072712af942b641abb8f958
GGLONTI1 <giorgiglonti23@gmail.com>
2024-09-24 19:23:08 +0400

    prisma category works

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,11 @@
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "./connect"
+
 
 export const authOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
